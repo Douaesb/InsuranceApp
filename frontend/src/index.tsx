6dd47ac4491a8ddceb6ce9a1397cc5c88cdef21e
Douae sb <douae.sb411@gmail.com>
2025-03-10 12:27:43 +0000

    auth frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import { ThemeProvider } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
+import App from "./App"
 import theme from "./theme"
+import { AuthProvider } from "./contexts/AuthContext"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
@@ -11,6 +13,9 @@ root.render(
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
